93cca5384271102b5b879c639fa69be49942297c
Adil Muhammed k <adilmhd4539@gamil.com>
2025-04-21 00:12:53 +0530

    inital commit

---
+++ routes/todos.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router = Router();
+
+const todos: number[] = [];
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+export default router;
